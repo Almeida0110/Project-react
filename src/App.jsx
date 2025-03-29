5db2c5167cb2a0b1b50ffc11ec6a97e8dcacee4a
Almeida0110 <abnerkaikesilvaalmeida@gmail.com>
2025-03-28 20:13:51 -0400

    Primeiro commit do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Message from './components/Message.jsx';
+
+
+function App() {
+  return ( 
+    <div>
+  <h1>Hello, Abner!</h1>
+  <Message text="React" text2="Angular" />
+  </div>
+  )
+}
+
+export default App;
---
+++ src/components/Message.jsx
@@ -0,0 +1,5 @@
+function Message(props){
+    return <h2>Este é meu primeiro codigo {props.text} - {props.text2} </h2>;
+}
+
+export default Message;
